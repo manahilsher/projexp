ff05701db92b0b2697e1278e5a8670153269e817
Manahil Sher <manahil.sher@gmail.com>
2020-03-21 16:42:35 -0400

    first commit

---
+++ src/main/java/com/sher/projexp/ProjexpApplication.java
@@ -0,0 +1,13 @@
+package com.sher.projexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sher/projexp/ProjexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sher.projexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
